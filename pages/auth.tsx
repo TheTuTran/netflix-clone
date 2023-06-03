4060335fe8d1e28a0fe4f916952c7bdd9c781482
TheTuTran <131922588+TheTuTran@users.noreply.github.com>
2023-06-02 21:08:26 -0500

    fixed more errors

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { signIn } from 'next-auth/react';
+import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from "next/router";
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
