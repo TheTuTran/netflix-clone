d1cad92bfeebecbbd294d6ebb64c570e88765564
TheTuTran <131922588+TheTuTran@users.noreply.github.com>
2023-09-02 14:19:54 -0500

    capitalize file names

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,44 @@
-import { getSession } from 'next-auth/react';
-import { NextPageContext } from 'next';
-import Navbar from '@/components/Navbar';
-import BillBoard from '@/components/Billboard';
-import MovieList from '@/components/MovieList';
-import useMovieList from '@/hooks/useMovieList';
-import useFavorites from '@/hooks/useFavorites';
-import InfoModal from '@/components/InfoModal';
-import useInfoModal from '@/hooks/useInfoModal';
+import { getSession } from "next-auth/react";
+import { NextPageContext } from "next";
+import Navbar from "@/components/Navbar";
+import BillBoard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
+import useInfoModal from "@/hooks/useInfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
-	const session = await getSession(context);
-	
-	if (!session) {
-		return {
-		redirect: {
-			destination: '/auth',
-			permanent: false,
-		}
-		}
-	}
+  const session = await getSession(context);
 
-	return {
-		props: {}
-	}
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
 
 export default function Home() {
-	const { data: movies = [] } = useMovieList();
-	const { data: favorites = [] } = useFavorites();
-	const { isOpen, closeModal } = useInfoModal();
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
-	return (
-	<>
-		<InfoModal visible={isOpen} onClose={closeModal}/>
-		<Navbar />
-		<BillBoard />
-		<div className="pb-40">
-			<MovieList data={movies} title="Trending Now"/>
-			<MovieList data={favorites} title="My List"/>
-		</div>
-	</>
-	)
-};
+  return (
+    <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <BillBoard />
+      <div className="pb-40">
+        <MovieList data={movies} title="Trending Now" />
+        <MovieList data={favorites} title="My List" />
+      </div>
+    </>
+  );
+}
